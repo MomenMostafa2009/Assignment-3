d25b2d3084f3652b724f2a061c63a8335575758b
MomenMostafa2009 <momendarsh2009@gmail.com>
2024-12-19 02:51:46 +0200

    Q5 solved

--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -68,6 +68,45 @@ namespace Assignment_3
         //    return sum;
         //}
         #endregion
+        #region Q5
+        //static void Main()
+        //{
+        //    Console.Write("Enter a number: ");
+        //    int number = int.Parse(Console.ReadLine());
+
+        //    bool isPrime = IsPrime(number);
+
+        //    if (isPrime)
+        //    {
+        //        Console.WriteLine($"{number} is a prime number.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"{number} is not a prime number.");
+        //    }
+        //}
+
+        //static bool IsPrime(int number)
+        //{
+        //    if (number <= 1)
+        //    {
+        //        return false;
+        //    }
+
+        //    for (int i = 2; i <= Math.Sqrt(number); i++)
+        //    {
+        //        if (number % i == 0)
+        //        {
+        //            return false; 
+        //        }
+        //    }
+
+
+        //    return true;
+        //}
+
+
+        #endregion
     }
 }
 
