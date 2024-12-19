bccebf154d4dbbe5ece806fdd394abd3a114b40d
MomenMostafa2009 <momendarsh2009@gmail.com>
2024-12-19 02:48:53 +0200

    Q3 solved

--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -10,6 +10,39 @@ namespace Assignment_3
         #region Q2
         //i cant how to explain iam so sorry
         #endregion
+        #region Q3
+        //static (double, double, double, double) SummationAndSubtraction(double a, double b, double c, double d)
+        //{
+        //    double sum1 = a + b;
+        //    double diff1 = a - b;
+
+        //    double sum2 = c + d;
+        //    double diff2 = c - d;
+
+        //    return (sum1, diff1, sum2, diff2);
+        //}
+        //static void Main()
+        //{
+        //    Console.Write("fir num: ");
+        //    double num1 = double.Parse(Console.ReadLine());
+
+        //    Console.Write("sec num: ");
+        //    double num2 = double.Parse(Console.ReadLine());
+
+        //    Console.Write("thi num: ");
+        //    double num3 = double.Parse(Console.ReadLine());
+
+        //    Console.Write("four num: ");
+        //    double num4 = double.Parse(Console.ReadLine());
+
+        //    (double sum1, double diff1, double sum2, double diff2) = SummationAndSubtraction(num1, num2, num3, num4);
+
+        //    Console.WriteLine($"Sum of first two numbers: {sum1}");
+        //    Console.WriteLine($"Difference of first two numbers: {diff1}");
+        //    Console.WriteLine($"Sum of next two numbers: {sum2}");
+        //    Console.WriteLine($"Difference of next two numbers: {diff2}");
+        //}
+        #endregion
     }
 }
 
